ef90918fa72fe2b99a892d823b78238f05943508
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2022-07-18 13:29:21 +0900

    if 조건문 | if,  if else, 중첩 조건문, if else if 조건문 공부하기
    if 조건문에 관련된 조건문 공부
    홀수 짝수 구분하는 조건문과 계절을 구분하는 조건문은 조건을 생각해보는 데에 도움이 많이 되니, 또 복습해보자!
    
    <혼자 공부하는 자바스크립트>, 윤인성

---
+++ 조건문 연습 예제/if else_정말 간단한 if조건문 예제.js
@@ -0,0 +1,8 @@
+const date = new Date()
+const hour = date.getHours()
+
+if (hour < 12){
+  alert('오전입니다.')
+} else {
+  alert('오후입니다.')
+}
---
+++ 조건문 연습 예제/if if_정말 간단한 if조건문 예제.js
@@ -0,0 +1,10 @@
+const date = new Date()
+const hour = date.getHours()
+
+if (hour < 12){
+  alert('오전입니다.');
+}
+
+if (hour >= 12){
+  alert('오후입니다.')
+}
